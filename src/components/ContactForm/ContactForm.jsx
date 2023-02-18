803e7c41a17e6471c13435b843443cddc281ac55
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-18 20:46:08 +0200

    task-02 updated

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -17,6 +17,14 @@ class ContactForm extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addContact(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
   };
 
   render() {
@@ -31,6 +39,7 @@ class ContactForm extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              value={this.state.name}
               onChange={this.onInputChange}
               required
             />
@@ -43,6 +52,7 @@ class ContactForm extends Component {
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              value={this.state.number}
               onChange={this.onInputChange}
               required
             />
